b4ec6d0644120dc38814bf7ddba5d00d05faab5c
Karolina S <karshi@kth.se>
2021-02-23 13:18:36 +0100

    fix .json

--- js/load_data.js
+++ js/load_data.js
@@ -2,28 +2,30 @@ export function load_data(){
     var links = [];
     var nodes = [];
 
-    d3.json('../backend/steam_scrapy/new_users.json', function(data) {
-        var user = data[0]; // main user
-        user.playtime.forEach(game => {    // link user to owned games
-            var size = game.playtime/100; // decide size of game-node
-            links.push({'source':user.steamid, 'target':game.game_name, 'width':2});
-            nodes.push({'id':game.game_name, 'name':game.game_name, 'type':2, 'size':size}) // type 2: user-owned game
-        });
-        nodes.push({'id':user.steamid, 'name':user.personaname, 'type':1, 'size':5}); // type 1: user
-    })
+    d3.json('../backend/steam_scrapy/new_users.json')
+        .then(function(data) {
+            var user = data[0]; // main user
+            user.playtime.forEach(game => {    // link user to owned games
+                var size = game.playtime/100; // decide size of game-node
+                links.push({'source':user.steamid, 'target':game.game_name, 'width':2});
+                nodes.push({'id':game.game_name, 'name':game.game_name, 'type':2, 'size':size}) // type 2: user-owned game
+            });
+            nodes.push({'id':user.steamid, 'name':user.personaname, 'type':1, 'size':5}); // type 1: user
+        })
 
-    d3.json('../backend/steam_scrapy/connection.json', function(data) {
-        var user_games = nodes.filter(game => game.type == 2);  // get user owned games
+    d3.json('../backend/steam_scrapy/connection.json')
+        .then(function(data) {
+            var user_games = nodes.filter(game => game.type == 2);  // get user owned games
 
-        // show 50 first games
-        for(var i = 0; i < 100; i++) {
-            if( (user_games.find(game => game.name == data.nodes[i].name)) == undefined) { // if not owned by user, add node to graph
-                //console.log(data.nodes[i].name, " is not owned.");
-                nodes.push({'id':data.nodes[i].name, 'name':data.nodes[i].name, 'type':3, 'size':5}) // type 3: not owned
-            }else {
-                //console.log(data.nodes[i].name, " IS owned.");
+            // show 50 first games
+            for(var i = 0; i < 100; i++) {
+                if( (user_games.find(game => game.name == data.nodes[i].name)) == undefined) { // if not owned by user, add node to graph
+                    //console.log(data.nodes[i].name, " is not owned.");
+                    nodes.push({'id':data.nodes[i].name, 'name':data.nodes[i].name, 'type':3, 'size':5}) // type 3: not owned
+                }else {
+                    //console.log(data.nodes[i].name, " IS owned.");
+                }
             }
-        }
 
         // generate connections with the nodes we have
         for(var i = 0; i < data.links.length; i++) {
