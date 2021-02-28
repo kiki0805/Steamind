45ebb7c4f90d15c736e0ca5440a9f18b868ec6be
Zezhe Huang <zezhe@kth.se>
2021-02-28 22:15:50 +0100

    add playtime in games.json

--- backend/steam_scrapy/utils.py
+++ backend/steam_scrapy/utils.py
@@ -16,6 +16,27 @@ def dump_games():
         data.append(single)
     return data
 
+def dump_games_for_user(user):
+    games = Game.select().where(Game.name!="")
+    data = []
+    for game in games:
+        single = model_to_dict(game, backrefs=True, max_depth=2)
+        single.pop('publishers')
+        single.pop('review_set')
+        genres = [g["genre"]['description'] for g in single.pop('genreprops_set')]
+        single['genres'] = genres
+        tags = [t['tag']['name'] for t in single.pop('tagged_set')]
+        single['tags'] = tags
+        single.pop('playtime_set')
+
+        query = Playtime.select().where(Playtime.user==user, Playtime.game==game)
+        if query.exists():
+            single['playtime'] = query.first().time
+        else:
+            single['playtime'] = -1
+        data.append(single)
+    return data
+
 
 def dump_users():
     users = User.select().where(User.personaname!="")
