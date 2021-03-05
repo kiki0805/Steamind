3cc70f3b907f84fc361fea4047751e399e771c55
Xinyi Wang <is.xinyiwang@outlook.com>
2021-03-05 10:37:59 +0100

    add info icon

---
+++ assets/js/bootstrap.tooltip.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({
+    title: "<h3>Info</h3><p>content</p>",
+    html: true,
+    placement: 'left',
+    animation: true,
+    delay:{
+      show:100,
+      hide:100
+    },
+
+  })
+})
+
