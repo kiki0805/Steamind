5eacd23aa9c259e8218a3b1034fb285242cfe042
Karolina S <karshi@kth.se>
2021-03-11 14:18:38 +0100

    hover on category to display name

--- js/filterviz.js
+++ js/filterviz.js
@@ -224,7 +224,7 @@ function viz(tree) {
             .transition().duration(400)
             .style('opacity', 1)
             .style('display', 'block')
-            .style('left', (d3.event.pageX - 100) + "px")
+            .style('left', (d3.event.pageX - 150) + "px")
             .style('top', (d3.event.pageY - 60) + "px");
     }
 
